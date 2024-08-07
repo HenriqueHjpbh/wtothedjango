2266194da840b1486be72c03a055e2dda26913b1
Henrique Pereira <80718496+HenriqueHjpbh@users.noreply.github.com>
2024-08-07 19:27:44 -0300

    Update settings.py

--- wtothedjango/settings.py
+++ wtothedjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dt&je2o3u=v=c(n5_r&4j)in^65i-eyis$@me9lfr$w@)xjdr#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
 
 # Application definition
